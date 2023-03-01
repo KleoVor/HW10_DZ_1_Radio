6da30e34dbe691ea41ae1f146edd1f715a06d566
KleoVor <t9671253993@gmail.com>
2023-03-01 12:07:16 +0300

    change RADIO

--- src/test/java/ru.netology/RadioTest.java
+++ src/test/java/ru.netology/RadioTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-
 public class RadioTest {
 
     @ParameterizedTest // зададим максимальое кол-во станций через конструктор
